4224661bc706ecb9cd15f965bb0bbddd2cfe2972
johnathan carstens <jcarstensedu@gmail.com>
2023-04-06 11:58:58 -0600

    all test pass

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = (num1 * num2)
+
+
+let random = 0
+random++;
+
+
+let num3 = 4
+let num4 = 5
+let mod = (num3 % num4)
+
+
+Math.max(1, 2, 3, 20)
+let max = 20
